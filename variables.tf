3f2d0a643d5ecd88091f3107fede0b1158435e5b
pmoghle <110378952+pmoghle@users.noreply.github.com>
2023-01-04 10:52:16 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "aws_access_key" {
-default = “<your_access_key>”
+default = “<AKIAQPEKF2BZAWQMHXIZ>”
 }
 variable "aws_secret_key" {
-default = “<your_secret_key>”
+default = “<pwDKlRjIIy7eAoFCvGo6CMTDwsL7xydLu3Y2HeOc>”
  }
 variable "region" {
-    default = "region"
+    default = "ap-south-1"
 }
